336ad7f88ab7785d208c797e2695f3781fe26d8c
lenkton <lenktongm@gmail.com>
2020-06-18 21:26:03 +0500

    project initialization

--- ReadersWriters/ReadersWriters.cpp
+++ ReadersWriters/ReadersWriters.cpp
@@ -1,20 +1,36 @@
-﻿// ReadersWriters.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <thread>
+#include <vector>
 
-#include <iostream>
+#define READERS_NUM 5
+#define WRITERS_NUM 2
+
+void writerMain(){}
+
+void readerMain(){
+	std::cout << "a";
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	////threads initialization
+	//readers initialization
+	std::vector<std::thread> readers;
+	for (int i = 0; i < READERS_NUM; i++)
+	{
+		std::thread t(readerMain);
+		readers.push_back(std::move(t));
+	}
+
+	//writers initialization
+	std::vector<std::thread> writers;
+	for (int i = 0; i < WRITERS_NUM; i++)
+	{
+		std::thread t(readerMain);
+		writers.push_back(std::move(t));
+	}
+
+	////waiting for threads to finish
+	for (auto& a : readers)a.join();
+	for (auto& a : writers)a.join();
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
